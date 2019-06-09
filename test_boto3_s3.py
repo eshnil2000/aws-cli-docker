0e78dd7029bec5b06b5738acf6da09fbb41b4205
Nick <eshnil2000@gmail.com>
2019-06-08 20:58:05 -0700

    Update test_boto3_s3.py

--- test_boto3_s3.py
+++ test_boto3_s3.py
@@ -1,4 +1,19 @@
-import boto3
+mport boto3
 s3 = boto3.resource('s3')
+s3client = boto3.client('s3')
+
 for bucket in s3.buckets.all():
         print(bucket.name)
+        for obj in bucket.objects.all():
+                print(obj.key)
+
+print(s3)
+print(s3client)
+#for bucket in s3.buckets.all():
+#       print(bucket.name)
+#       theobjects = s3client.list_objects_v2(Bucket=bucket[bucket.name])
+#       for object in theobjects["Contents"]:
+#               print(object["Key"])
+
+# Boto 3
+s3.Object(bucket.name, 'hello.txt').put(Body=open('/project/hello.txt', 'rb'))
